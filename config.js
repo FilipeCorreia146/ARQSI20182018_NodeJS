77019c328cde2765aa25ab253364c89af4f8455a
Bruna Moreira Teixeira <1150595@isep.ipp.pt>
2017-11-05 12:10:20 +0000

    config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+var jwt = require('jsonwebtoken');
+
+module.exports = {
+    
+    'jwt': jwt,
+    'secret': 'ilovescotchyscotch',
+    'database': 'mongodb://1150524:arqsi2017@ds040837.mlab.com:40837/arqsi'
+    
+};
